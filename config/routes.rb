52a2f8d549ec6d05ca654c8f8d172d9cd362876d
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-05-02 17:21:58 -0400

    add devise users, and teacher/student/admin models which inherit from them

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :students
+  devise_for :teachers
+  devise_for :admins
+  get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "pages#index"
+  namespace :dashboard do
+    root to: "dashboard#index"
+  end
 end

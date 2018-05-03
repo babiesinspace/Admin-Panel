8d2c9ecdf8c32ae6bdf2982b6dc073af8c93083f
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-05-03 17:56:48 -0400

    namespaced routes. all users have a dashboard but different views are available based on type of user

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,31 @@
 Rails.application.routes.draw do
-  devise_for :students
-  devise_for :teachers
-  devise_for :admins
-  get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "pages#index"
   namespace :dashboard do
+    authenticated :student do
+        resources :cohorts, module: "student", :only => [:show, :index] do 
+          resources :grades, :only => [:show, :index]
+          resources :assignments, :only => [:show, :index]
+          resources :announcements, :only => [:show, :index]
+        end
+    end
+
+    authenticated :teacher do
+        resources :cohorts, module: "teacher", :only => [:show, :index] do 
+          resources :grades
+          resources :assignments
+          resources :announcements
+        end
+    end
+
+    authenticated :admin do
+        resources :courses, module: "admin" do 
+          resources :cohorts
+          resources :announcements
+        end
+    end
+
     root to: "dashboard#index"
   end
 end

bc2597afc522b8872fcb6a80564cfc3cf019cc75
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-05-11 18:06:16 -0400

    just got crud functionality for teachers/assignments via ajax. Great. Only 10BILLION to go..

--- app/controllers/dashboard/teacher/assignments_controller.rb
+++ app/controllers/dashboard/teacher/assignments_controller.rb
@@ -21,30 +21,44 @@ class Dashboard::Teacher::AssignmentsController < ApplicationController
 
     def edit
       @assignment = Assignment.find(params[:id])
-      
+      respond_to do |format|
+        format.html
+        format.js
+      end
     end 
 
     def update
       @assignment = Assignment.find(params[:id])
+      @cohort = @assignment.cohort
       if @assignment.update_attributes(assignment_params)
         # Handle a successful update.
-        redirect_to dashboard_cohort_assignment_path(@assignment.cohort_id, @assignment.id)
+        respond_to do |format|
+          format.html { redirect_to dashboard_cohort_assignment_path(@assignment.cohort_id, @assignment.id) }
+          format.js
+        end
       else
         render 'edit'
       end
     end
 
     def new
-      @cohort = params[:cohort_id]
-      @assignment = Assignment.new
+      @cohortid = params[:cohort_id]
+      respond_to do |format|
+        format.html
+        format.js
+      end
     end
 
     def create
+      @cohort = Cohort.find(params[:cohort_id])
       @assignment = Assignment.new(assignment_params)
-      @assignment.cohort_id = params[:cohort_id]
+      @assignment.cohort = @cohort 
       if @assignment.save
-        # Handle a successful update.
-        redirect_to dashboard_cohort_assignments_path
+        # Handle a successful creation
+        respond_to do |format|
+          format.html { redirect_to dashboard_cohort_assignments_path(@assignment.cohort_id)}
+          format.js
+        end
       else
         render 'new'
       end
@@ -54,7 +68,10 @@ class Dashboard::Teacher::AssignmentsController < ApplicationController
       @assignment = Assignment.find(params[:id])
       @assignment.destroy
       @cohort = Cohort.find(params[:cohort_id])
-      redirect_to dashboard_cohort_assignments_path(@cohort)
+      respond_to do |format|
+        format.html {redirect_to dashboard_cohort_assignments_path(@cohort)}
+        format.js
+      end
     end 
 
     private
